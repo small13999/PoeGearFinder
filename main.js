9e32f13ed2b73a8b88f427f252b153292198b6c4
Kiss Ármin Donát <xk2g4r@inf.elte.hu>
2024-02-10 18:47:21 +0100

    get item weights sum

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const WEIGHT = 0;
+const THRESHOLD = 1;
+
 function randRange(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
@@ -38,6 +41,75 @@ function GenerateRandomGear(itemCount) {
     }
 }
 
-GenerateRandomGear(10);
-console.log(items);
+const weights = {
+    "Dex": {
+        value: 150,
+        type: THRESHOLD
+    },
+    "Str": {
+        value: 150,
+        type: THRESHOLD
+    },
+    "Int": {
+        value: 100,
+        type: THRESHOLD
+    },
+    "Suppress": {
+        value: 100,
+        type: THRESHOLD
+    },
+    "Health": {
+        value: 1.0,
+        type: WEIGHT
+    },
+    "Damage": {
+        value: 0.7,
+        type: WEIGHT
+    },
+    "Fire Res": {
+        value: 75,
+        type: THRESHOLD
+    },
+    "Cold Res": {
+        value: 75,
+        type: THRESHOLD
+    },
+    "Lightning Res": {
+        value: 75,
+        type: THRESHOLD
+    },
+}
+
+function getItemsWeightSum(items, weights, cutoff) {
+    console.log("hi")
+    items.forEach(item => {
+        let sum = 0;
+        for (const property in weights) {
+            if (!item[property]) {
+                continue;
+            }
+            const modInd = modTypes.indexOf(property);
+            if (weights[property].type === THRESHOLD) {
+                sum += parseInt(item[property]) / parseInt(modRangeMax[modInd]);
+            } else {
+                sum += parseInt(item[property]) / parseInt(modRangeMax[modInd]) * parseInt(weights[property].value);
+            }
+        }
+        item["WeightSum"] = sum;
+    })
+
+    const maxWeightSum = Math.max(...items.map(o => o.WeightSum));
+    const ret = [];
+    items.forEach(item => {
+        if (item["WeightSum"] > maxWeightSum * cutoff) {
+            ret.push(item);
+        }
+    });
+
+    return ret;
+}
+
+GenerateRandomGear(100);
+const bestItems = getItemsWeightSum(items, weights, 0.9);
+console.log(bestItems);
 
